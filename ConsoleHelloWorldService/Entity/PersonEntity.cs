bdc42b7d4de633899745a63661dd626c28e2aaf3
bxstar <yurongsheng@163.com>
2015-07-21 08:06:35 +0800

    加入接口定义文件

---
+++ ConsoleHelloWorldService/Entity/PersonEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleHelloWorldService.Entity
+{
+    /// <summary>
+    /// 人员基础信息
+    /// </summary>
+    public class PersonEntity
+    {
+        /// <summary>
+        /// 人员ID，唯一标识
+        /// </summary>
+        public long ID { get; set; }
+
+        /// <summary>
+        /// 人员名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 人员年龄
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
---
+++ ConsoleHelloWorldService/InterfaceDefine/IHelloWorldServiceDefine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleHelloWorldService.Entity;
+
+namespace ConsoleHelloWorldService.InterfaceDefine
+{
+    /// <summary>
+    /// HelloWorld接口定义
+    /// </summary>
+    public interface IHelloWorldServiceDefine
+    {
+        PersonEntity SavePerson(PersonEntity p); 
+    }
+}
